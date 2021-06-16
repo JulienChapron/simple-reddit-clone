584498b279adf62cd41756ae4fb53ba6cdc40b59
Julien Chapron <j.chapron@pm.me>
2021-06-16 08:57:41 +0200

    change backgroundcolor description in subreddit

--- client/src/components/SubredditCard.jsx
+++ client/src/components/SubredditCard.jsx
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { authContext } from '../contexts/Auth';
 import { getPublic } from '../utils/RequestPublic';
-import UploadAvatar from './UploadAvatar';
-import UploadBackground from './UploadBackground';
+import categories from '../assets/categories/Categories';
 
 const SubredditCard = (props) => {
   const { auth } = useContext(authContext);
   const [subreddit, setSubreddit] = useState(null);
+  const [backgrounColorCategory, setBackgrounColorCategory] = useState(null);
   const [admin, setAdmin] = useState(false);
   const getSubreddit = async () => {
     const params = window.location.href.split('/');
     const subreddit = params[1];
     try {
       const response = await getPublic('subreddits/' + subreddit);
-      setSubreddit(response.data[0]);
-      if (response.data[0] && response.data[0].userId === auth.data.data._id) setAdmin(true);
+      categories.map((category) => {
+        if (response.data[0].category === category.name.toLowerCase()) {
+          setBackgrounColorCategory(category.color);
+        }
+      });
+      if (response.data[0]) setSubreddit(response.data[0]);
+      if (response.data[0] && response.data[0].userId === auth.data.data._id)
+        setAdmin(true);
     } catch (error) {
       console.log(error);
     }
@@ -28,9 +34,12 @@ const SubredditCard = (props) => {
     <div>
       {subreddit !== null && subreddit !== undefined ? (
         <div style={{ padding: '0px' }} className="card-reddit">
-          <UploadBackground data={subreddit} admin={admin} type="subreddits" />
-          <UploadAvatar data={subreddit} admin={admin} type="subreddits" />
-          <div style={{ marginTop: '40px', padding: '10px' }}>
+          <div
+            style={{ backgroundColor: backgrounColorCategory, padding: '10px' }}
+          >
+            About the community
+          </div>
+          <div style={{ padding: '10px' }}>
             <p style={{ minHeight: '30px' }}>{subreddit.description}</p>
             <p style={{ minHeight: '30px' }}>0 members</p>
             {admin ? (
